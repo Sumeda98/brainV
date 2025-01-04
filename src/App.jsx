ea4b665569e8c266d9e5ae76fe77904e18c2e195
Sumedha Bandarigoda <90858067+Sumeda98@users.noreply.github.com>
2025-01-04 13:23:03 +0530

    tailwind theme setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import './App.css'
 
 function App() {
-
   return (
     <>
-      <h1 className="text-3xl text-center text-red-700">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl text-center">Hello world!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

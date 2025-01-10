d032bb4e79b25705e06590f58f34eee69ed71307
Sumedha Bandarigoda <90858067+Sumeda98@users.noreply.github.com>
2025-01-10 11:08:58 +0530

    designs components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,18 @@ import { useLocation } from 'react-router-dom';
 import Button from './Button';
 
 const Header = () => {
+  
     const pathname = useLocation();
+    
   return (
     <>
-      <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+      <div className="fixed top-0 left-0 z-50 w-full border-b bg-n-8/90 backdrop-blur-sm border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className='block w-[12rem] xl:mr-8' href='#hero'>
             <img src={brainV} width={190} height={40}/>
         </a>
         <nav className='hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent'>
-            <div className='relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row'>
+            <div className='relative flex flex-col items-center justify-center m-auto z-2 lg:flex-row'>
                 {navigation.map((item) =>(
                     <a key={item.id} href={item.url} className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${item.onlyMobile? 'lg:hidden' : ''} px-6 py-6 md:py-8 lg:mr-0.25 lg:text-sm lg:font-semibold ${item.url === pathname.hash ? "z-2 lg:text-n-1":"lg:text-n-1/50"} lg:leading-5 lg:hover:text-n-1 xl:px-12`} >
                         {item.title}
@@ -21,7 +23,7 @@ const Header = () => {
                 ))}
             </div>
         </nav>
-        <a href='#signup' className='button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block'>New account</a>
+        <a href='#signup' className='hidden mr-8 transition-colors button text-n-1/50 hover:text-n-1 lg:block'>New account</a>
         <Button className='hidden lg:flex' href="#login">Sign in</Button>
       </div>
       </div>
